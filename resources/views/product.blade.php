3eec5f3714af182fc0f3223c6c72079526f5fa2d
Mursalin-Dowla <justjim844@gmail.com>
2023-04-01 16:17:13 +0600

    Refined

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -5,10 +5,12 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Practice Api</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 <body class="p-5">
+    <h6 class="text-center">Product Api Access For Practice: <span class="text-primary">https://jim.ovshopbd.com/api/viewapi</span></h6>
+    <a href="https://github.com/Mursalin-Dowla/Practice_Api_L/blob/main/practiceapi_documentation%20by%20Jim.docx" target="_blank" class="btn btn-info">Documentation</a>
     <h1>Products</h1>
    
     <table class="table" style="border:1px solid black;">
--- routes/web.php
+++ routes/web.php
@@ -16,9 +16,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+
+Route::get('/',[ProductController::class, 'index']);
 
 Route::get('/products',[ProductController::class, 'index']);
 Route::get('/addproducts',[ApiBladeController::class, 'index'])->name('blade.addproduct');
